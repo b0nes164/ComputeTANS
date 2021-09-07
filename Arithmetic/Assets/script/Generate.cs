cfdb1278dd46bf9e32f30b03009c9daead0ca79b
b0nes164 <68340554+b0nes164@users.noreply.github.com>
2021-09-07 13:41:53 -0700

    Init

--- Arithmetic/Assets/script/Generate.cs
+++ Arithmetic/Assets/script/Generate.cs
@@ -1,18 +1,11 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using Unity.Jobs;
+using Unity.Burst;
+using Unity.Collections;
 
-public class Generate : MonoBehaviour
+public class Generate 
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
 
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
 }
